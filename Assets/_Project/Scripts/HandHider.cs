a2857b682dffc69c00743515470afb4c71d2507d
ArturoSerranoDev <asm.arturo.developer@gmail.com>
2021-12-03 18:34:59 +0100

    Created Hand Hider to hide hands when selecting with an interactor

---
+++ Assets/_Project/Scripts/HandHider.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR;
+
+public class HandHider : MonoBehaviour
+{
+    [SerializeField] private List<XRBaseInteractor> interactors;
+
+    private void OnEnable()
+    {
+        foreach (XRBaseInteractor interactor in interactors)
+        {
+            interactor.selectEntered.AddListener(HideHand);
+            interactor.selectExited.AddListener(ShowHand);
+        }
+    }
+
+    private void ShowHand(SelectExitEventArgs exitEventArgs)
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    private void HideHand(SelectEnterEventArgs enterEventArgs)
+    {
+        this.gameObject.SetActive(false);
+    }
+}
