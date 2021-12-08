bac945b00e30ed45409eb37fe801de231c263473
ArturoInnerspace <arturo.serrano@innerspace.eu>
2021-12-08 15:22:22 +0100

    Started created cannon interactable

---
+++ Assets/_Project/Scripts/Interactables/CannonInteractable.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonInteractable : MonoBehaviour
+{
+  // VARIABLES
+  
+  // Things to do
+  
+  // Able to rotate itself if main Dial is moved
+  
+  // Able to rotate the cannon depending of input of middle handle
+  
+  // Able to move forwards or backwards with wheel handle
+  
+  
+}
